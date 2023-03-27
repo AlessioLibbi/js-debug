888b942199109fca4f6e383c624879f71d210ca4
Alessio Libbi <alex.libbi@gmail.com>
2023-03-27 17:05:14 +0200

    Add:end part

--- bundle-2/main.js
+++ bundle-2/main.js
@@ -11,37 +11,47 @@
 
 
 // ESERCIZIO 1
-for (let i = 0; i > 5; i++) {
+for (let i = 0; i < 5; i++) {
     console.log(i);
 }
 
+// 1- questo era infame
 
 // ESERCIZIO 2
-function addIfEven(num) {
-    if (num % 2 = 0) {
+function addIfEven(num = 16) {
+    if (num % 2 === 0) {
         return num + 5;
     }
     return num;
 }
+console.log(addIfEven());
 
+// 1- ti torna se pari il numero +5 se dispari solo il numero
+// 2- l errore era l uguaglianza e la funzione senza alcun tipo di valore
 
-// ESERCIZIO 3
+// // ESERCIZIO 3
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 0; i < 5; i++) {
         console.log(i);
     }
 }
+loopToFive()
+
+// 1- ti fa un ciclo finche non i e uguale a 5
+// 2- l eroore stava nella punteggiatura del ciclo e che la funzione non fosse richiamata fuori
 
 
-// ESERCIZIO 4 (suggerimento: ci sono 8 errori)
+// // ESERCIZIO 4 (suggerimento: ci sono 8 errori)
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
+    for (let i = 0; i <= numbers.length - 1; i++) {
+        if (numbers[i] % 2 === 0) {
+            evenNumbers.push(numbers[i]);
         }
-        return evenNumbers;
     }
+    console.log(evenNumbers);
+    return evenNumbers;
 }
 displayEvenNumbers(); // [2,4,6,8]
+console.log(displayEvenNumbers());
